cc4f0647fc5ef911911bb90eb9f63e8e2cf1ff90
DzungVu <dzungvucs@gmail.com>
2018-01-12 09:56:46 +0700

    Update code

--- main.py
+++
@@ -1,8 +0,0 @@
-import RunLengthText
-from HuffmanCoding import HuffmanCoding
-
-# RunLengthText.encodeMultiFile('data', True)
-
-path = 'data\\TheFurnishedRoom.txt'
-h = HuffmanCoding(path)
-h.compress()
